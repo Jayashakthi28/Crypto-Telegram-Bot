0e2ea6d41170620669ad407c4845afa2e1eb5639
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2022-09-29 07:44:37 +0530

    update greet py

--- greet.py
+++ greet.py
@@ -7,36 +7,36 @@ import database as sqler
 import requests
 import time
 while 1:
-    bot = telebot.TeleBot(api.tak, parse_mode='HTML')
-    sqler.list_updater()
-    IST = pytz.timezone('Asia/Kolkata')
-    datetime_ist = datetime.datetime.now(IST).strftime('%H')
-    greet_list=[]
-    for x in sqler.id_list:
-        greet_list.append(x)
-    for x in sqler.login_list:
-        greet_list.append(x)
-    greet_list=set(greet_list)
-    for chat_id in greet_list:
-        url='https://inspiration.goprogram.co.uk'
-        time.sleep(0.01)
-        response=requests.get(url=url)
-        data=response.json()
-        if(datetime_ist=='8'):
-            text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Morning! Have a nice Day...</b>'
-            bot.send_message(chat_id,text)
-        elif(datetime_ist=="13"):
-            text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Afternoon! Work Hard...</b>'
-            bot.send_message(chat_id,text)
-        elif(datetime_ist=="17"):
-            text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Evening! Relax Your Day...</b>'
-            bot.send_message(chat_id,text)
-        if(datetime_ist=="20"):
-            text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Night! Sweet Dreams...</b>'
-        try:   
-            bot.send_message(chat_id,text)
-        except:
-            pass
+    try:
+        bot = telebot.TeleBot(api.tak, parse_mode='HTML')
+        sqler.list_updater()
+        IST = pytz.timezone('Asia/Kolkata')
+        datetime_ist = datetime.datetime.now(IST).strftime('%H')
+        greet_list=[]
+        for x in sqler.id_list:
+            greet_list.append(x)
+        for x in sqler.login_list:
+            greet_list.append(x)
+        greet_list=set(greet_list)
+        for chat_id in greet_list:
+            url='https://inspiration.goprogram.co.uk'
+            time.sleep(0.01)
+            response=requests.get(url=url)
+            data=response.json()
+            if(datetime_ist=='8'):
+                text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Morning! Have a nice Day...</b>'
+                bot.send_message(chat_id,text)
+            elif(datetime_ist=="13"):
+                text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Afternoon! Work Hard...</b>'
+                bot.send_message(chat_id,text)
+            elif(datetime_ist=="17"):
+                text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Evening! Relax Your Day...</b>'
+                bot.send_message(chat_id,text)
+            if(datetime_ist=="20"):
+                text=f'<b><i>{data["quote"]}</i></b>\n\n<b>Good Night! Sweet Dreams...</b>'
+                bot.send_message(chat_id,text)
+    except:
+        pass
     time.sleep(320)
     sqler.list_updater()
     for chat_id in sqler.login_list:
